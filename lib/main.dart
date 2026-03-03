770b28c1a51f90611784ddefc55111e8cd940103
Tanhh <tienanh0516@gmail.com>
2026-03-03 23:21:49 +1100

    Week1-Screen4-updatedv2

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-  import 'package:flutter/material.dart';
-
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          child: Center(
-            child: Text(
-              "BingeFlix",
+        body: Container(color: Colors.teal,
+          alignment: Alignment.center,
+          child: Text("BINGEFLIX", 
+          style: TextStyle(
+            fontFamily: "Bebas Neue",
+            fontWeight: FontWeight.w600,
+            fontSize: 80,
             ),
           ),
-          color: Colors.teal, // background color
         ),
       ),
     ),
